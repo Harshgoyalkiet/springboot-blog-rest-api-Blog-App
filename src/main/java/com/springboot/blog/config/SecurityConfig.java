4efb209d9f8f8aa152016d269f6d66f00cb25dd5
harsh Goyal <harshgoyalg@gmail.com>
2025-11-24 00:28:24 +0530

    In memory based authentication

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +16,20 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest()
-                        .authenticated())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
-
     // In basic Authentication we need to pass the username and password in headers of the requests.
+
+    // creating In memory user based authentication with roles.
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails ramesh = User.builder().username("ramesh").password("ramesh").roles("USER").build();
+        UserDetails admin = User.builder().username("ADMIN").password("ADMIN").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(ramesh,admin);
+    }
 
 
 }
